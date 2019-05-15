902edcc38403706d0f4de68d22afe53e1fa6a56e
zlj <390057892@qq.com>
2019-05-15 17:53:52 +0800

    up

--- lib/views/page/home_page.dart
+++ lib/views/page/home_page.dart
@@ -14,7 +14,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final HomeBloc bloc = BlocProvider.of<HomeBloc>(context);
-    bloc.getData(labelId: labelId);
+    bloc.homeSubject.listen((event) {
+//      if (labelId == event.labelId) {
+//        _controller.sendBack(false, event.status);
+//      }
+    });
     return ListView(
       children: <Widget>[
         SwipeDiy(),
